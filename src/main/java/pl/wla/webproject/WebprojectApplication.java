02a4717c5c57460bc13ad6d95cc5c6138118a6cb
Wojtek L. <wojteklad@tlen.pl>
2023-11-30 19:02:25 +0100

    commit no 20 base auth

--- src/main/java/pl/wla/webproject/WebprojectApplication.java
+++ src/main/java/pl/wla/webproject/WebprojectApplication.java
@@ -4,7 +4,6 @@ package pl.wla.webproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication(scanBasePackages = "pl.wla.webproject")
 public class WebprojectApplication {
     public static void main(String[] args) {
